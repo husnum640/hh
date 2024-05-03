f05f88e14f1294245d039bd355fd53486a9255c7
husnum640 <168820847+husnum640@users.noreply.github.com>
2024-05-03 15:24:04 +0400

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -97,7 +97,7 @@ HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 # Yenilənmə
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/sahibziko/delta.git")
+    "https://github.com/husnum640/hh.git")
 
 # Konsol
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
